24edb6ae36258f3cbec4c8fff8a1d3ab10be881f
Bartu Onder <onderbartu8@gmail.com>
2025-12-29 21:26:15 +0300

    list methods are used

---
+++ ListMethods.py
@@ -0,0 +1,34 @@
+#all List methods
+myList = [1,2,3,4,5,6]
+print(myList)
+
+myList.append(7)
+print(myList)
+
+myList.insert(1,8)
+print(myList)
+
+myList.pop()
+print(myList)
+
+print(myList.index(5))
+
+myList.remove(5)
+print(myList)
+
+myList.sort()
+print(myList)
+
+myList.reverse()
+print(myList)
+
+myList.extend([1,2,3,4,5,6])
+print(myList)
+
+print(myList.count(5))
+
+myList2 = myList.copy()
+print(myList2)
+
+myList.clear()
+print(myList)
